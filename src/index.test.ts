9952a2534e105c222e58de882ef434e08bf33683
Marcus Rådell <marcus@radell.net>
2024-05-16 08:49:44 +0200

    file reply.txt gets written to

--- src/index.test.ts
+++ src/index.test.ts
@@ -8,3 +8,9 @@ test("Read file should contain the text 'Hi!'", () => {
   const result = textContent.split("\n")[0];
   expect(result).toEqual("Hi!");
 });
+
+test("File reply.txt should be written to", () => {
+  const result = fs.writeFileSync("reply.txt", "Goddag goddag!\n");
+
+  expect(() => result).not.toThrow();
+});
